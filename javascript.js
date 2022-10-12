9e4fb94224cc9d88d79ac42ed63e822f04f2bf22
xsdra <sandraxsdra@gmail.com>
2022-10-12 12:45:47 +0300

    pushing work

---
+++ javascript.js
@@ -0,0 +1,13 @@
+
+        /* THIS IS JAVASCRIPT*/
+        document.getElementById("heading").innerHTML=" lord ";
+      /* GGKG
+      GHHH
+      GGGG */
+      var text;
+      text=I am a billionaire";
+
+      var answer;
+      answer="t'sup";
+
+      let response=" lord
